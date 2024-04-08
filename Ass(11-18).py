1b9c02c83369634207faff2aa0cd47dc089e872e
Ramy Medhat <ramymedhat97@gmail.com>
2024-04-08 17:58:39 +0200

    Tasks 1-4

---
+++ Ass(11-18).py
@@ -0,0 +1,33 @@
+# Task 1
+Name = "Ramy"
+Age = "25"
+Country = "Egypt"
+
+print("Hello '", Name, "', How You Doing \\\"\" Your Age Is \"", Age, "\" + And Your Country Is:", Country)
+
+
+# Task 2
+print("Hello '", Name, "', How You Doing \\\"\"  \nYour Age Is \"", Age, "\" + \nAnd Your Country Is:", Country)
+
+# Task 3
+
+name = 'Elzero'
+# Needed Output
+# Second Letter Is "l"
+print(name[1])
+# Third Letter Is "z"
+print(name[2])
+# Last Letter Is "o"
+print(name[-1])
+
+# Task 4
+name = 'Elzero'
+# Needed Output
+# "lze"
+print(name[1:4])
+# "Ezr"
+print(name[::2])
+# "rzE"
+print(name[-2::-2])
+
+# Task 5
