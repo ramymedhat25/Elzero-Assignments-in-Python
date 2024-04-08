0e09b4e9825946d4e5b1d1e2f293c2b68049ff64
Ramy Medhat <ramymedhat97@gmail.com>
2024-04-08 20:22:38 +0200

    Task 8

--- Ass(11-18).py
+++ Ass(11-18).py
@@ -54,4 +54,15 @@ name_one = "Osama"
 print(name_one.rjust(20,"@"))
 
 name_two = "Osama_Elzero"
-print(name_two.rjust(20,"@"))
+print(name_two.rjust(20,"@")) 
+
+# Task 8
+name_one = "OSamA"
+name_two = "osaMA"
+
+# Needed Output
+# osAMa
+# OSAma
+
+print(name_one.swapcase())
+print(name_two.swapcase())
