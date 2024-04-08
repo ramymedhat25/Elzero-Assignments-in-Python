cf24137ebb3cc439b6e67194995f9280cff3d3f7
Ramy Medhat <ramymedhat97@gmail.com>
2024-04-08 18:23:47 +0200

    Task 6

--- Ass(11-18).py
+++ Ass(11-18).py
@@ -35,3 +35,16 @@ print(name[-2::-2])
 name = "#@#@Elzero#@#@"
 
 print(name.strip("@#"))
+
+# Task 6
+
+num = "9"
+print(num.zfill(4))
+num = "15"
+print(num.zfill(4))
+num = "130"
+print(num.zfill(4))
+num = "950"
+print(num.zfill(4))
+num = "1500"
+print(num.zfill(4))
