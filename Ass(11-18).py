2c3d5ce02e0b59a0e1efbbff2ff88eb74d6af8fa
Ramy Medhat <ramymedhat97@gmail.com>
2024-04-08 20:53:20 +0200

    task 12

--- Ass(11-18).py
+++ Ass(11-18).py
@@ -86,3 +86,10 @@ msg = "I <3 Python And Although <3 Elzero Web School"
 
 print(msg.replace('<3', 'Love', 1))
 
+# Task 12
+msg = "I <3 Python And Although <3 Elzero Web School"
+
+# Needed Output
+# I Love Python And Although Love Elzero Web School
+print(msg.replace('<3', 'Love'))
+
