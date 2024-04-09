780164894d3b60cf90d1c22a99912f2101e3fe4f
Ramy Medhat <ramymedhat97@gmail.com>
2024-04-09 17:02:09 +0200

    Task 2

--- Ass(19-20).py
+++ Ass(19-20).py
@@ -3,3 +3,11 @@ print(int(1))
 print(float(1))
 print(complex(1))
 
+# Task 2
+complex=1+2j
+
+# Print Imaginary Part Here
+# Print Real Part Here
+
+print("Real Part IS {}".format(complex.real)) 
+
