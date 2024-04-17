dcb5752c729d0e1dbd71c229e8e4de84814fbfba
Ramy Medhat <ramymedhat97@gmail.com>
2024-04-17 13:23:33 +0200

    task 4

--- Ass(21-23).py
+++ Ass(21-23).py
@@ -42,3 +42,8 @@ friends = ["Osama", "Ahmed", "Sayed", "Ali", "Mahmoud"]
 
 # Needed Output
 # ["Osama", "Ahmed", "Sayed", "Elzero", "Elzero"]
+
+friends[-2:] = ["Elzero", "Elzero"]
+
+print(friends)
+
